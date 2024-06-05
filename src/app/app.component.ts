46f0c3a19b70f1e0caf3efeef59ba22c883b51e3
NogueiraDiaz <153636410+NogueiraDiaz@users.noreply.github.com>
2024-06-05 14:46:43 +0200

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,11 @@ export class AppComponent implements OnInit {
   coches: any[] = [];
   currentPage: number = 1;
   itemsPerPage: number = 8;
-  editedCoche: any = { marca: '', modelo: '', anio: '' };
+  editedCoche: any = { marca: '', modelo: '', año: '' };
   mostrarFormulario: boolean = false;
   mostrarFormulario2: boolean = false;
   mostrarFormulario3: boolean = false;
+  mostrarFormulario4: boolean = false;
   nombre: string = '';
   fecha: string = '';
   fechaFinal: string = '';
@@ -25,12 +26,25 @@ export class AppComponent implements OnInit {
   marcaOptions: string[] = [];
   colorOptions: string[] = [];
 
+  nuevoCoche: any = {
+    marca: '',
+    modelo: '',
+    año: null,
+    color: '',
+    matricula: '',
+    precio_diario: null,
+    alquiler: null,
+    imagen: '',
+    nota: '',
+    kilometraje: null
+  };
+
   filters: any = {
     precioMin: 0,
     precioMax: 1000,
     color: '',
     marca: '',
-    anio: '',
+    año: '',
     estadoAlquiler: '',
     clienteNombre: ''
   };
@@ -74,7 +88,7 @@ export class AppComponent implements OnInit {
         coche.precio_diario <= this.filters.precioMax &&
         (this.filters.color === '' || coche.color === this.filters.color) &&
         (this.filters.marca === '' || coche.marca === this.filters.marca) &&
-        (this.filters.anio === '' || coche.anio === this.filters.anio) &&
+        (this.filters.año === '' || coche.año === this.filters.año) &&
         (this.filters.estadoAlquiler === '' || (this.filters.estadoAlquiler === 'Sí' && coche.alquiler) || (this.filters.estadoAlquiler === 'No' && !coche.alquiler)) &&
         (this.filters.clienteNombre === '' || (coche.clienteNombre && coche.clienteNombre.includes(this.filters.clienteNombre)))
       );
@@ -96,9 +110,10 @@ export class AppComponent implements OnInit {
       this.editedCoche = { ...coche };
       this.mostrarFormulario = false;
     } else {
-      if (this.mostrarFormulario2 || this.mostrarFormulario3) {
+      if (this.mostrarFormulario2 || this.mostrarFormulario3 || this.mostrarFormulario4) {
         this.mostrarFormulario2 = false;
         this.mostrarFormulario3 = false;
+        this.mostrarFormulario4 = false;
       }
       this.editedCoche = { ...coche };
       this.mostrarFormulario = true;
@@ -112,9 +127,10 @@ export class AppComponent implements OnInit {
       this.editedCoche = { ...coche };
       this.mostrarFormulario2 = false;
     } else {
-      if (this.mostrarFormulario || this.mostrarFormulario3) {
+      if (this.mostrarFormulario || this.mostrarFormulario3 || this.mostrarFormulario4) {
         this.mostrarFormulario = false;
         this.mostrarFormulario3 = false;
+        this.mostrarFormulario4 = false;
       }
       this.editedCoche = { ...coche };
       this.mostrarFormulario2 = true;
@@ -126,12 +142,28 @@ export class AppComponent implements OnInit {
       this.editedCoche = { ...coche };
       this.mostrarFormulario3 = false;
     } else {
-      if (this.mostrarFormulario || this.mostrarFormulario2) {
+      if (this.mostrarFormulario || this.mostrarFormulario2 || this.mostrarFormulario4) {
         this.mostrarFormulario = false;
         this.mostrarFormulario2 = false;
+        this.mostrarFormulario4 = false;
       }
       this.editedCoche = { ...coche };
       this.mostrarFormulario3 = true;
+    }
+  }
+
+  openEditForm4(coche: any) {
+    if (this.mostrarFormulario4) {
+      this.editedCoche = { ...coche };
+      this.mostrarFormulario4 = false;
+    } else {
+      if (this.mostrarFormulario || this.mostrarFormulario2 || this.mostrarFormulario3) {
+        this.mostrarFormulario = false;
+        this.mostrarFormulario2 = false;
+        this.mostrarFormulario3 = false;
+      }
+      this.editedCoche = { ...coche };
+      this.mostrarFormulario4 = true;
     }
   }
 
@@ -143,10 +175,18 @@ export class AppComponent implements OnInit {
     return `${yyyy}-${mm}-${dd}`;
   }
 
-  confirmarReserva() {
-    const cocheId = this.editedCoche.id; // Obtener el ID del coche
+  alquilarCoche() {
+    
+    
     const nombreCliente = (<HTMLInputElement>document.getElementById('nombre')).value;
     const fechaInicio = (<HTMLInputElement>document.getElementById('fecha')).value;
+
+    if (!nombreCliente || !fechaInicio) {
+      alert('Por favor, complete todos los campos requeridos.');
+      return; // Detener la ejecución si los campos requeridos no están llenos
+  }
+
+    const cocheId = this.editedCoche.id; // Obtener el ID del coche
     this.editedCoche.alquiler = {
       clienteNombre: nombreCliente,
       fechaInicio: fechaInicio
@@ -179,10 +219,17 @@ export class AppComponent implements OnInit {
   
 
   confirmarDevolucion() {
-    const cocheId = this.editedCoche.id; // Obtener el ID del coche
-    this.editedCoche.alquiler = null;
+    
     const fechaFinalString = (<HTMLInputElement>document.getElementById('fechaFinal')).value;
+
+    if (!fechaFinalString) {
+      alert('Por favor, complete todos los campos requeridos.');
+      return; // Detener la ejecución si los campos requeridos no están llenos
+  }
+
+    this.editedCoche.alquiler = null;
     const fechaFinal = new Date(fechaFinalString);
+    const cocheId = this.editedCoche.id; // Obtener el ID del coche
 
     // Lógica para confirmar la devolución del vehículo
     // Aquí debes enviar los datos a tu backend para actualizar la reserva
@@ -198,7 +245,7 @@ export class AppComponent implements OnInit {
       .then(response => {
         if (response.ok) {
           console.log('¡Devolución confirmada correctamente!');
-          this.calcularMontoDevolucion(cocheId, fechaFinal); // Calcular el monto adeudado
+          this.calcularPrecio(cocheId, fechaFinal); // Calcular el monto adeudado
           this.fetchCoches(); // Volver a cargar la lista de coches después de confirmar la devolución
           this.mostrarFormulario3 = false; // Ocultar el formulario
         } else {
@@ -210,7 +257,7 @@ export class AppComponent implements OnInit {
       });
   }
 
-  calcularMontoDevolucion(cocheId: number, fechaFinal: Date) {
+  calcularPrecio(cocheId: number, fechaFinal: Date) {
     const coche = this.coches.find(coche => coche.id === cocheId);
     if (coche && coche.alquiler && coche.alquiler.fechaInicio) {
       const fechaInicio = new Date(coche.alquiler.fechaInicio);
@@ -223,14 +270,73 @@ export class AppComponent implements OnInit {
     }
   }
 
+  insertarCoche() {
+    // Verificar si todos los campos requeridos están llenos
+    if (!this.nuevoCoche.marca || !this.nuevoCoche.modelo || !this.nuevoCoche.año || !this.nuevoCoche.color || !this.nuevoCoche.matricula || !this.nuevoCoche.precio_diario || !this.nuevoCoche.imagen || !this.nuevoCoche.precio_diario || !this.nuevoCoche.kilometraje) {
+      alert('Por favor, introduzca todos los campos requeridos.');
+      return; // Evitar insertar el coche si algún campo requerido está vacío
+    }
+  
+    // Llenar el objeto nuevoCoche con los valores de los campos del formulario
+    this.nuevoCoche.marca = (<HTMLInputElement>document.getElementById('marca')).value;
+    this.nuevoCoche.modelo = (<HTMLInputElement>document.getElementById('modelo')).value;
+    this.nuevoCoche.año = (<HTMLInputElement>document.getElementById('año')).value;
+    this.nuevoCoche.color = (<HTMLInputElement>document.getElementById('color')).value;
+    this.nuevoCoche.matricula = (<HTMLInputElement>document.getElementById('matricula')).value;
+    this.nuevoCoche.precio_diario = (<HTMLInputElement>document.getElementById('precio_diario')).value;
+    this.nuevoCoche.imagen = (<HTMLInputElement>document.getElementById('imagen')).value;
+    this.nuevoCoche.nota = (<HTMLInputElement>document.getElementById('nota')).value;
+    this.nuevoCoche.kilometraje = (<HTMLInputElement>document.getElementById('kilometraje')).value;
+  
+    // Lógica para confirmar el alquiler del vehículo
+    // Aquí debes enviar los datos a tu backend para actualizar el coche
+    fetch(`http://localhost:3000/coches`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.nuevoCoche)
+    })
+    .then(response => {
+      if (response.ok) {
+        console.log('Coche introducido correctamente!');
+        this.mostrarFormulario4 = false; // Ocultar el formulario
+        this.fetchCoches(); // Volver a cargar la lista de coches después de confirmar el alquiler
+        // Vaciar los campos del formulario
+        this.nuevoCoche = {
+          marca: '',
+          modelo: '',
+          año: null,
+          color: '',
+          matricula: '',
+          precio_diario: null,
+          alquiler: null,
+          imagen: '',
+          nota: '',
+          kilometraje: null
+        };
+      } else {
+        console.error('Error al introducir el vehículo.');
+      }
+    })
+    .catch(error => {
+      console.error('Error al introducir el vehículo: ', error);
+    });
+  }
 
-
-  guardarCambios() {
+  editarCoche() {
     this.editedCoche.marca = (<HTMLInputElement>document.getElementById('marca')).value;
     this.editedCoche.modelo = (<HTMLInputElement>document.getElementById('modelo')).value;
-    this.editedCoche.anio = (<HTMLInputElement>document.getElementById('anio')).value;
+    this.editedCoche.año = (<HTMLInputElement>document.getElementById('año')).value;
+    
+    if (!this.editedCoche.marca || !this.editedCoche.modelo || !this.editedCoche.año) {
+      alert('Por favor, complete todos los campos requeridos.');
+      return; // Detener la ejecución si los campos requeridos no están llenos
+  }
 
-    fetch(`http://localhost:3000/coches/${this.editedCoche.id}`, {
+  const cocheId = this.editedCoche.id; // Obtener el ID del coche
+
+    fetch(`http://localhost:3000/coches/${cocheId}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
